e80b167c03faf70a9170fd73a15115af8482f5f1
aliceinnets <aliceinnets@gmail.com>
2017-07-27 18:21:43 +0200

    tried to crawling with auth

--- src/aliceinnets/web/apps/CrawlAndDownload.java
+++ src/aliceinnets/web/apps/CrawlAndDownload.java
@@ -1,7 +1,17 @@
 package aliceinnets.web.apps;
 
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.jsoup.Jsoup;
+import org.jsoup.helper.HttpConnection;
+import org.jsoup.nodes.Document;
 
 import aliceinnets.util.OneLiners;
 import aliceinnets.web.WebUtils;
@@ -15,7 +25,55 @@ public class CrawlAndDownload {
 	
 	public static void main(String[] args) {
 //		saveAllMp3FromHmix();
-		saveAllZipFromSlos();
+//		saveAllZipFromSlos();
+		saveAllFiles("http://grammars.grlmc.com/DeepLearn2017/materials/#", "");
+	}
+	
+	public static void saveAllFiles(String url, String folder) {
+//		OneLiners.mkdirs(folder);
+		
+		try {
+			Document document = Jsoup.connect(url).get();
+			System.out.println(document);
+			System.out.println(document.getElementById("userName"));
+			System.out.println(document.getElementById("userPassword"));
+			System.out.println(document.getElementById("userName").attr("placeholder", URLEncoder.encode("sehyun.kwak@ipp.mpg.de", "UTF-8")));
+			System.out.println(document.getElementById("userPassword").attr("placeholder", URLEncoder.encode("a95c08f043", "UTF-8")));
+			
+			System.out.println(document);
+			
+			
+			URL url2 = new URL(url);
+			URLConnection connection = url2.openConnection();
+			connection.setDoOutput(true);
+			
+			DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+			wr.writeBytes(document.toString());
+			wr.flush();
+			wr.close();
+
+			String responseCode = connection.getContentType();
+			System.out.println("\nSending 'POST' request to URL : " + url);
+			System.out.println("Post parameters : " + document.toString());
+			System.out.println("Response Code : " + responseCode);
+			Document document2 = Jsoup.parse(url2, 30000);
+			System.out.println(document2);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+//		System.exit(1);
+		Spider spider = new Spider(url);
+		spider.crawl(100);
+		
+		for(String page : spider.getPagesVisited()) {
+			System.out.println(page);
+//			if(page.equals(url)) continue;
+//			
+//			if(!page.contains("#")) {
+//				saveAll(page, folder, "mp3", 3);
+//			}
+		}
 	}
 	
 	public static void saveAllZipFromSlos() {
